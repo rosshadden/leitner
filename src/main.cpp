fddec8f14e6bd5cd2fc5c83c8778e5ad3e3d3a3a
Ross Hadden <rosshadden@gmail.com>
2023-11-08 14:39:32 -0500

    feat: working

--- src/main.cpp
+++ src/main.cpp
@@ -4,14 +4,21 @@
 #include "secrets.h"
 
 const int btnPin = 2;
+const int ledPin = LED_BUILTIN;
 
 char ssid[] = WIFI_SSID;
 char pass[] = WIFI_PASS;
+char host[] = API_HOST;
+int port = API_PORT;
+char token[] = HA_TOKEN;
 
 int status = WL_IDLE_STATUS;
 bool busy = false;
 
 WiFiClient client;
+
+String payload = "{ \"entity_id\": \"input_boolean.debug\" }";
+// String payload = "{ \"entity_id\": \"switch.bedroom_lights\" }";
 
 void printWifiStatus() {
 	Serial.print("\nSSID: ");
@@ -23,9 +30,32 @@ void printWifiStatus() {
 	Serial.println(" dBm\n");
 }
 
+void makeRequest() {
+	Serial.println("connecting server...");
+	if (client.connect(host, port)) {
+		Serial.print("connected: ");
+		Serial.println(host);
+		Serial.println();
+
+		// client.println("POST /api/services/switch/toggle");
+		client.println("POST /api/services/input_boolean/toggle");
+		client.print("Host: ");
+		client.println(host);
+		client.println("Content-Type: application/json");
+		client.print("Authorization: Bearer ");
+		client.println(token);
+		client.print("Content-Length: ");
+		client.println(payload.length());
+		client.println();
+		client.println(payload);
+		client.println("Connection: close");
+		client.println();
+	}
+}
+
 void setup() {
 	pinMode(btnPin, INPUT_PULLUP);
-	pinMode(LED_BUILTIN, OUTPUT);
+	pinMode(ledPin, OUTPUT);
 
 	Serial.begin(9600);
 
@@ -34,20 +64,34 @@ void setup() {
 	Serial.println("starting...");
 
 	while (status != WL_CONNECTED) {
-		Serial.print("connecting: ");
-		Serial.println(ssid);
+		Serial.println("connecting wifi...");
 		status = WiFi.begin(ssid, pass);
 		delay(1000);
 	}
 
+	// TODO: blink while connecting
+	digitalWrite(ledPin, HIGH);
 	printWifiStatus();
 }
 
 void loop() {
 	if (!busy && digitalRead(btnPin) == LOW) {
 		busy = true;
+		makeRequest();
 		delay(10);
 	} else if (busy && digitalRead(btnPin) == HIGH) {
+		while (client.available()) {
+			char c = client.read();
+			Serial.write(c);
+		}
+		Serial.println();
+
+		if (!client.connected()) {
+			Serial.println("disconnecting server...\n");
+			client.stop();
+			Serial.end();
+		}
+
 		busy = false;
 		delay(10);
 	}
